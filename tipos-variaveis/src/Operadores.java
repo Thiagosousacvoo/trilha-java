77c6e1846d5c5e26c1223308fef98a3325b4199b
ThiagoSousa <thiagosousacvoo@gmail.com>
2024-09-26 13:58:46 -0300

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Operadores {
+    public static void main(String[] args) {
+       
+        int a, b;
+        a = 10;
+        b = 1;
+
+        String resultado = a==b ?"Verdadeiro" :"Falso";
+        System.out.println(resultado);
+    }    
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
+import java.util.Date;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // byte idade = 123;
+        // short ano = 2021;
+        // int cep = 21070333;
+        // long cpf = 98765432109L;
+        // float pi = 3.14F;
+        // double salario = 1275.33;
     }
 }
